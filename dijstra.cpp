ce282a1d52173df27c8480076fa2b5b6f16587c9
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-03-15 18:52:26 +0530

    Update dijstra.cpp

--- dijstra.cpp
+++ dijstra.cpp
@@ -1,46 +1,26 @@
-// code by CasioFx-69
-int main()
-{
-    int V;
-    cin >> V;
-    vector<pair<int, int>> adj[V];
-    int e;
-    cin >> e;
-    for (int i = 0; i < e; i++)
+vector <int> dijkstra(int V, vector<pair<int,int>> adj[], int S)
     {
-        int x, y, z;
-        cin >> x >> y >> z;
-        adj[x].push_back({z, y});
-        adj[y].push_back({z, x});
-    }
-    int src;
-    cin >> src;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
-    q.push({0, 0});
-    vector<int> visited(V, 0);
-    vector<int>dis(V);
-    dis[0]=0;
-    while (q.size())
-    {
-        auto x = q.top();
-        q.pop();
-        if (visited[x.second] == 1)
-        {
-            continue;
-        }
-        else
-        {
-            visited[x.second]=1;
-            dis[x.second] = x.first;
-            int t = x.second;
-            for (auto i : adj[x.second])
-            {
-                if(visited[i.second]==0)
-                q.push(make_pair(x.first + i.first, i.second));
-
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        q.push({0,S});
+        vector<int>distance(V);
+        vector<int>visited(V);
+        while(q.size()){
+            auto x = q.top();
+            q.pop();
+            int srcw = x.first;
+            int srcidx = x.second;
+            if(visited[srcidx]==1){
+                continue;
+            }
+            else{
+                distance[srcidx] = srcw;
+                visited[srcidx]=1;
+                for(auto i:adj[srcidx]){
+                    if(visited[i.second]==0){
+                        q.push({srcw+i.first,i.second});
+                    }
+                }
             }
         }
+        return distance;
     }
-    printv(dis);
-    return 0;
-}
