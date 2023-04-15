7e3d53e4e11e312f8fbc3b81a0e1dacdd5184aff
jay jariwala <94626218+jay10125104@users.noreply.github.com>
2023-04-15 23:12:21 +0530

    Update dijstra.cpp

--- dijstra.cpp
+++ dijstra.cpp
@@ -1,1 +1,25 @@
-
+vector <int> dijkstra(int V, vector<pair<int,int>> adj[], int S){
+      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+      q.push({0,S});
+      vector<int>distance(V,INT_MAX);
+      vector<int>visited(V);
+      while(q.size()){
+          auto x = q.top();
+          q.pop();
+          int srcw = x.first;
+          int srcidx = x.second;
+          if(visited[srcidx]==1){
+              continue;
+          }
+          else{
+              distance[srcidx] = srcw;
+              visited[srcidx]=1;
+              for(auto i:adj[srcidx]){
+                  if(visited[i.second]==0){
+                      q.push({srcw+i.first,i.second});
+                  }
+              }
+          }
+      }
+      return distance;
+}
